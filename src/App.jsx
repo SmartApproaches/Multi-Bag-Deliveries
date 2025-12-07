0f500db952df1bb5acb0d23e61cf5e02e666aa76
Simi <similoluwabonafide@gmail.com>
2025-12-07 23:32:57 +0000

    hero section commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Home from "./pages/home";
+import About from "./pages/about";
+import Services from "./pages/services";
+import Contact from "./pages/contact";
+import Signup from "./pages/auth/signup";
+import Login from "./pages/auth/login";
 
 function App() {
   return (
@@ -8,6 +13,11 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </>
