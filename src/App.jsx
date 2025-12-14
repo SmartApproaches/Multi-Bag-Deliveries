1553cb1a6c703a98037028a86bce24c2effff2d5
Simi <similoluwabonafide@gmail.com>
2025-12-14 23:20:06 +0000

    make-your-work-easier commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Home from "./pages/home";
+import MakeWorkEasier from "./components/button/MakeWorkEasier";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <MakeWorkEasier/>
     </>
   );
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,18 @@
 import logo from "../assets/logo.svg";
+import img1 from "../assets/img1.svg";
+import img2 from "../assets/img2.svg";
+import img3 from "../assets/img3.svg";
+import img4 from "../assets/img4.svg";
+import img5 from "../assets/img5.svg";
+import img6 from "../assets/img6.svg";
 
 export const IMAGES = {
   logo: logo,
+  img1: img1,
+  img2: img2,
+  img3: img3,
+  img4: img4,
+  img5: img5,
+  img6: img6,
   // Add other images here as needed
 };
