bb1fcdb0237e4cbe2aff20fd1916d25b962108ba
Simi <similoluwabonafide@gmail.com>
2025-12-18 02:23:02 +0000

    delivery-options commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Home from "./pages/home";
+import DeliveryOptions from "./components/button/DeliveryOptions";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <DeliveryOptions />
     </>
   );
 }
