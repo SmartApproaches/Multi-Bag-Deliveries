3c941c97a1427101e1951be2bad2449dae3a99de
alaobasit <alaooluwapelumi222@gmail.com>
2025-12-20 21:23:42 +0100

    commit all changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import CreateAccount from "./components/CreateAccount";
-import LoginModal from "./components/LoginModal";
-import PaymentSuccess from "./components/PaymentSuccess";
+
 
  function App () {
   return (
     <>
     <CreateAccount />
 
-    <LoginModal />
-
-    <PaymentSuccess />
+    
 
     </>
   )
