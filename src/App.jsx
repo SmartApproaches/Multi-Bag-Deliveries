4ec4db69ecb6dadfda6b5828174810b32e7132a2
Simi <similoluwabonafide@gmail.com>
2025-12-29 02:17:54 +0000

    How it works commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Home from "./pages/home";
+import HowItWorks from "./components/button/HowItWorks";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
         </Routes>
+        <HowItWorks />
       </Router>
     </>
   );
