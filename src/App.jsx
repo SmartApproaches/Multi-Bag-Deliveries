84fa171920cd39736ad3ade54936902f6fc696e1
abdul damilola <abduldamilola79@gmail.com>
2026-01-06 23:01:40 +0100

    parcel commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router";
-import Home from "./pages/home";
+import Parcel from './components/Parcel';
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+     <Parcel />
     </>
   );
 }
