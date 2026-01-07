e4d3cf55eff4ccbf5c8d4d1522a9d27c43d5c448
alaobasit <alaooluwapelumi222@gmail.com>
2026-01-07 12:17:55 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,57 @@
-import Parcel from './components/Parcel';
 import React from "react";
-import Bookadelivery_step4 from "./components/button/Bookadelivery_step4";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-   
-import Bookadelivery from "./components/button/Bookadelivery";
-import Payment from "./components/Payment";
-import PaymentSuccess from "./components/PaymentSuccess";
-import LoginModal from "./components/LoginModal";
-import CreateAccount from "./components/CreateAccount";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+
+// Pages
 import Home from "./pages/home";
-import HowItWorks from "./components/button/HowItWorks";
+import LandingPage from "./pages/landing";
 import About from "./pages/about";
 import Services from "./pages/services";
 import Contact from "./pages/contact";
 import Signup from "./pages/auth/signup";
 import Login from "./pages/auth/login";
+
+// Global UI / modals
+import Bookadelivery_step4 from "./components/button/Bookadelivery_step4";
+import Payment from "./components/Payment";
+import Parcel from "./components/Parcel";
+import PaymentSuccess from "./components/PaymentSuccess";
+import LoginModal from "./components/LoginModal";
+import CreateAccount from "./components/CreateAccount";
 import DeliveryOptions from "./components/button/DeliveryOptions";
 import MakeWorkEasier from "./components/button/MakeWorkEasier";
 import SendOrReceive from "./components/button/SendOrReceive";
-import LandingPage from "./pages/landing";
+import HowItWorks from "./components/button/HowItWorks";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
-
-
-
       <Router>
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
           <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
-          {/* <Route path="/" element={<Home />} /> */}
-          {/*  <Bookadelivery_step4 />
-
-          <Payment />
-     <Parcel />
-          <PaymentSuccess />
-          <LoginModal />
-          <DeliveryOptions />
-          <CreateAccount />
-          <MakeWorkEasier />
-          <SendOrReceive />
-
-        <HowItWorks /> */}
         </Routes>
-      </Router>
 
+        {/* Global components (modals, floating UI, sections) */}
+        <Bookadelivery_step4 />
+        <Payment />
+        <PaymentSuccess />
+        <LoginModal />
+        <DeliveryOptions />
+        <CreateAccount />
+        <MakeWorkEasier />
+        <SendOrReceive />
+        <HowItWorks />
+
+        <Footer />
+      </Router>
     </>
   );
 }
