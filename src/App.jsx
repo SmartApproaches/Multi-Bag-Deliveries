7421c6e5232e330203051ed742cf433ac610490c
Toriola Taofeek <toriolataofeekolamilekan@gmail.com>
2026-01-08 23:32:51 +0100

    fix merge error

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,36 @@
-import Parcel from './components/Parcel';
 import React from "react";
-import BookadeliveryStep1 from "./components/button/Bookadelivery_step1";
-import Payment from "./components/Payment";
-import PaymentSuccess from "./components/PaymentSuccess";
-import LoginModal from './components/LoginModal';
-import CreateAccount from "./components/CreateAccount";
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
 
-import DeliveryOptions from "./components/button/DeliveryOptions";
-import MakeWorkEasier from "./components/button/MakeWorkEasier";
-import SendOrReceive from "./components/button/SendOrReceive";
+import MainLayout from "./layout";
+
+// Pages
 import LandingPage from "./pages/landing";
+import About from "./pages/about";
+import Services from "./pages/services";
+import Contact from "./pages/contact";
+import CreateAccount from "./components/CreateAccount";
+import Login from "./pages/auth/login";
+import Bookadelivery from "./components/button/Bookadelivery_step1";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
+    <Router>
+      <Routes>
+        {/* Routes WITH header & footer */}
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/signup" element={<CreateAccount />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/book-a-delivery" element={<Bookadelivery />} />
+        </Route>
 
-        {/* Global components (modals, floating UI, sections) */}
-        <BookadeliveryStep1 />
-        <Payment />
-        <PaymentSuccess />
-        <LoginModal />
-        <DeliveryOptions />
-        <CreateAccount />
-        <MakeWorkEasier />
-        <SendOrReceive />
-
-       
-      </Router>
-
-        <Footer />
-      </Router>
-    </>
+        {/* Routes WITHOUT header & footer */}
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
